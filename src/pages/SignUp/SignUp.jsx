14f42e24d3157424b5379c9be59e9043dcf82a0e
Lorena <blorena1321@gmail.com>
2023-11-29 12:15:07 -0300

    fix: connect back-end sign-up

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,37 +2,87 @@ import { styled } from "styled-components";
 import logo from "/src/assets/movielo-cut.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function SignUp(){
-    const [form, setForm] = useState({email: "", image: "", password: "", confirmPassword: ""});
-  
-    function handleForm(e){
-      setForm({...form, [e.target.name]: e.target.value});
-    }
-  
-      return(
-      <Wrapper>
-        <p>Sign Up</p>
-        <form>
-          <input
+function SignUp() {
+  const [form, setForm] = useState({
+    email: "",
+    profileImage: "",
+    name: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const navigate = useNavigate();
+
+  function handleForm(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function createUser(e) {
+    e.preventDefault();
+
+    const body = {
+      email: form.email,
+      profileImage: form.profileImage,
+      name: form.name,
+      username: form.username,
+      password: form.password,
+    };
+
+    const urlPost = `${import.meta.env.VITE_REACT_APP_API_URL}/sign-up`;
+    const promise = axios.post(urlPost, body);
+    promise.then(res => {
+      console.log("foi!")
+      navigate("/");
+    })
+    promise.catch(err => {
+      console.log(err.response.data.mensagem);
+    })
+  }
+
+  return (
+    <Wrapper>
+      <p>Sign Up</p>
+      <form onSubmit={createUser}>
+        <input
           required
           placeholder="Email"
           type="email"
           name="email"
           value={form.email}
           onChange={handleForm}
-          />
+        />
 
-          <input
+        <input
           required
           placeholder="Profile image"
           type="imagem"
-          name="image"
-          value={form.image}
+          name="profileImage"
+          value={form.profileImage}
           onChange={handleForm}
-          />
+        />
 
-          <input 
+        <input
+          required
+          placeholder="Name"
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleForm}
+        />
+
+        <input
+          required
+          placeholder="Username"
+          type="text"
+          name="username"
+          value={form.username}
+          onChange={handleForm}
+        />
+
+        <input
           required
           placeholder="Password"
           type="password"
@@ -40,9 +90,9 @@ function SignUp(){
           name="password"
           value={form.password}
           onChange={handleForm}
-          />
+        />
 
-          <input 
+        <input
           required
           placeholder="Confirm password"
           type="password"
@@ -50,81 +100,85 @@ function SignUp(){
           name="confirmPassword"
           value={form.confirmPassword}
           onChange={handleForm}
-          />
+        />
 
-          <button style={{background: "transparent"}}> 
-          <img src={logo} alt="Logo"/>
-          </button>
-        </form>
-        <span> <p>Already have a Movielo account?</p> <Link to={`/`}><button>Login</button></Link></span>
-      </Wrapper>
-      )
+        <button onClick={(e)=> createUser(e)} style={{ background: "transparent" }}>
+          <img src={logo} alt="Logo" />
+        </button>
+      </form>
+      <span>
+        {" "}
+        <p>Already have a Movielo account?</p>{" "}
+        <Link to={`/`}>
+          <button>Login</button>
+        </Link>
+      </span>
+    </Wrapper>
+  );
+}
+
+export default SignUp;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #222b31;
+
+  p {
+    font-family: "Tilt Neon", sans-serif;
+    color: #ffffff;
+    /* color: #fb921c; */
+    font-size: 35px;
   }
-  
-  export default SignUp;
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100vh;
-    background-color: #222b31;
-
-    p{
-      font-family: 'Tilt Neon', sans-serif;
-      color: #FFFFFF;
-      /* color: #fb921c; */
-      font-size: 35px;
-     
-    }
-
-    form {
+  form {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     margin-top: 50px;
-    }
+  }
 
-    input{
-      background-color: #343543;
-      width: 300px;
-      border: none;
-      border-radius: 8px;
-      padding: 10px;
-      outline: none;
-      color: #FFFFFF;
-      margin-top: 10px;
+  input {
+    background-color: #343543;
+    width: 300px;
+    border: none;
+    border-radius: 8px;
+    padding: 10px;
+    outline: none;
+    color: #ffffff;
+    margin-top: 10px;
+  }
+
+  button {
+    border: none;
+    margin-top: 40px;
+    cursor: pointer;
+    img {
+      width: 80px;
+    }
+  }
+
+  span {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+    p {
+      font-size: 12px;
     }
 
     button {
+      margin-top: 15px;
       border: none;
-      margin-top: 40px;
-      cursor: pointer;
-      img{
-        width: 80px;
-      }
+      padding: 7px;
+      background-color: #343543;
+      color: #ffffff;
+      border-radius: 5px;
     }
-
-    span{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-top: 20px;
-      p{
-        font-size: 12px;
-      }
-
-      button {
-        margin-top: 15px;
-        border: none;
-        padding: 7px;
-        background-color: #343543;
-        color: #FFFFFF;
-        border-radius: 5px;
-      }
-    }
-
-`
+  }
+`;
